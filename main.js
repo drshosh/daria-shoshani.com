caa0d572ce14815e141790514073abb7888a1faf
edenakshoti <edenakshoti1@gmail.com>
2026-08-04 14:02:50 +0200

    Temp: log full ImgBB response to diagnose URL field

--- main.js
+++ main.js
@@ -202,6 +202,7 @@
     if (!res.ok) throw new Error('ImgBB ' + res.status);
     const json = await res.json();
     if (!json.success) throw new Error('ImgBB: ' + (json.error?.message || 'upload failed'));
+    console.log('[imgbb-response]', JSON.stringify(json.data, null, 2));
     return json.data.url;
   };
 
